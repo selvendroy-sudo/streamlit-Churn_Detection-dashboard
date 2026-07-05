2e0b1ee9778779b307ab15cf49ab4a69facd30c3
Selvendroy <selvendroy@gmail.com>
2026-07-05 08:51:52 +0000

    Add Streamlit sales dashboard

--- streamlit-Churn_Detection-dashboard/dashboard/dashboard.py
+++ streamlit-Churn_Detection-dashboard/dashboard/dashboard.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-from pathlib import Path
+import plotly.express as px
 
 from sklearn.model_selection import train_test_split
 from sklearn.compose import ColumnTransformer
